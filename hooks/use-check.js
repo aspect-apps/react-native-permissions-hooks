c09c14b3ac4f08f07f8acfd485e3b9376255febe
Amogh J <amoghjahagirdargsf@gmail.com>
2020-11-01 10:57:49 +0400

    check and check Multiple combined

--- hooks/use-check.js
+++ hooks/use-check.js
@@ -3,17 +3,26 @@
  * @param {string} permission 
  * @param {array} update 
  */
-import {check, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {check,checkMultiple, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import {useEffect, useState} from 'react';
 
-export function useCheck(permission, update = []){
+export function useCheck(permission, update = [], isMultiple = true){
     const [result, setResult] = useState({result:null, type:'unknown'})
-    useEffect(() => {
+    if(!notMultiple){
+        useEffect(() => {
         check(permission).then((result) => setResult({result:result, type:'known'}))
-  .catch((error) => {
+    .catch((error) => {
     console.log(error)
 });
     },update)
+}else{
+        useEffect(() => {
+        checkMultiple(permission).then((result) => setResult({result:result, type:'known'}))
+    .catch((error) => {
+    console.log(error)
+});
+    },update)
+}
 
     return result;
 }
